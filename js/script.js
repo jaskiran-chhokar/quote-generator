6267b8b3f8b04c8eb40fd19d47f5dd0d1cade819
Jaskiran <jaskiranchhokar@gmail.com>
2019-11-14 19:43:34 -0500

    Update script.js
    add quotes array, and function that prints quote to console

--- js/script.js
+++ js/script.js
@@ -7,21 +7,64 @@ project 1 - A Random Quote Generator
   // Check the "Project Resources" section of the project instructions
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/*** 
- * `quotes` array 
-***/
 
+let quotes = [
+  {
+    'quote': 'An investment in knowledge pays the best interest.',
+    'source': 'Benjamin Franklin',
+    'citation': 'xxx',
+    'year': 2067
+  },
+  {
+    'quote': 'All that we see or seem is but a dream within a dream.',
+    'source': 'Edgar Allan Poe',
+    'citation': 'xxx',
+    'year': 2067
+  },
+  {
+    'quote': 'The supreme art of war is to subdue the enemy without fighting.',
+    'source': 'Sun Tzu',
+    'citation': 'xxx',
+    'year': 2067
+  },
+  {
+    'quote': 'The journey of a thousand miles begins with one step.',
+    'source': 'Lao Tzu',
+    'citation': 'xxx',
+    'year': 2067
+  },
+  {
+    'quote': 'If you cannot do great things, do small things in a great way.',
+    'source': 'Napoleon Hill',
+    'citation': 'xxx',
+    'year': 2067
+  }
+]; 
 
+console.log(quotes); 
 
-/***
- * `getRandomQuote` function
-***/
+// get random quote function 
+function getRandomQuote() {
+  let randomNumber = Math.floor(Math.random() * 4 + 1); 
+  return quotes[randomNumber];
+}
 
+console.log(getRandomQuote());
 
+// print quote function 
+function printQuote() {
+  let randomquote = getRandomQuote(); 
+  let quoteString = '<p class="quote">' + randomquote.quote + '</p>' + 
+                      '<p class="source">' +  randomquote.source + 
+                      '<span class="citation">' +  randomquote.citation +  '</span>' + 
+                      '<span class="year">' + randomquote.year + '</span>' + 
+                    '</p>'
 
-/***
- * `printQuote` function
-***/
+  return quoteString; 
+}
+
+console.log(printQuote());
+
 
 
 
